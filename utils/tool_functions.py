ae1ad4d9cd24326e2b9f827f16c76e1b11442e16
gonthier <maxjp.gr@gmail.com>
2024-03-26 10:35:31 -0500

    outline of functions

--- utils/tool_functions.py
+++ utils/tool_functions.py
@@ -4,6 +4,17 @@ from poibin import PoiBin
 import numpy as np
 import sys
 
+# Return the estimated time cost of chunking and replicating a data of 
+# size file_size into N chunks of size file_size/K
+# uses an interpolation or extrapolation from previous experiments
+# TODO in future works: update estimation with observation from current 
+# execution
+# Takes as inputs N, K, the size of the file and the bandwidth to write on the storage nodes
+# Return a time in seconds (or micro-seconds?)
+def replication_and_chuncking_time(N, K, file_size, bandwidths):
+	
+	return time
+
 # ~ # Faster than is_pareto_efficient_simple, but less readable.
 # ~ def is_pareto_efficient(costs, return_mask = True):
     # ~ """
@@ -31,9 +42,11 @@ import sys
         # ~ return is_efficient
 
 # Getting the set of N on the pareto front that match the reliability threshold
-def get_set_of_N_on_pareto_front(number_of_nodes, reliability_threshold, reliability_of_nodes, file_size):
+def get_set_of_N_on_pareto_front(number_of_nodes, reliability_threshold, reliability_of_nodes, file_size, bandwidths):
 	N_on_pareto = []
 	set_of_possible_N_and_K_couple = []
+	space_cost_of_couple = []
+	time_cost_of_couple = []
 	
 	# First we get the set of N and their associating K as big as possible that meet the resilience threshold
 	for i in range (1, number_of_nodes + 1):
@@ -41,9 +54,18 @@ def get_set_of_N_on_pareto_front(number_of_nodes, reliability_threshold, reliabi
 		if (K != -1): # This value of N can match the reliability threshold
 			set_of_possible_N_and_K_couple.append((i, K))
 	
+	print("set_of_possible_N_and_K_couple:", set_of_possible_N_and_K_couple)
+	
 	# Put in a table the time and space cost of each couple of possible N,K
-	# space cost = (file_size/K) * N
-	# time_cost = interpolation from Dante function 
+	
+	print(set_of_possible_N_and_K_couple[0][0], set_of_possible_N_and_K_couple[0][1])
+	
+	# ~ for i in range (0, size(set_of_possible_N_and_K_couple)):
+		# ~ space_cost_of_couple.append((file_size/K) * N)
+		# ~ time_cost_of_couple = replication_and_chuncking_time(set_of_possible_N_and_K_couple[i][0], set_of_possible_N_and_K_couple[i][1])
+		
+	# time_cost = interpolation from Dante function
+
 	
 	# Then we get from the possible couple the set of N that is on the pareto front
 	
@@ -71,12 +93,17 @@ def get_max_K_from_reliability_threshold_and_nodes(number_of_nodes, reliability_
 		print("No value of K can meet the reliability threshold with N =", number_of_nodes)
 	else:
 		print("Biggest K we can choose to meet the reliability threshold with N =", number_of_nodes, "is", max_K)
+	
 	return max_K
+
 
 # Under are just some values and examples on how to use the utils functions
 
 # Numpy arrays of probability of failure each node over the data timeframe
 p = np.array([0.01, 0.2, 0.1, 0.1, 0.1, 0.3])
+
+# Bandwidth to write on the storage nodes in MB/s
+bandwidths = np.array([10, 23, 15, 13, 11, 32])
 
 # Number of replica we create
 N = 6
@@ -91,5 +118,5 @@ print("Probability of availability must be superior to", reliability_threshold)
 
 K = get_max_K_from_reliability_threshold_and_nodes(N, reliability_threshold, p)
 
-set_of_N_on_pareto = get_set_of_N_on_pareto_front(N, reliability_threshold, p, file_size)
+set_of_N_on_pareto = get_set_of_N_on_pareto_front(N, reliability_threshold, p, file_size, bandwidths)
 print("The set of N on the pareto front between speed and size is", set_of_N_on_pareto)
