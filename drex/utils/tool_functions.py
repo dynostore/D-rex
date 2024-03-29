f716176e239b69f8226a4427b9d7d384c6c2e6f0
ddomizzisg <ddomizzisag@gmail.com>
2024-03-29 10:43:14 -0500

    Changes in directory structure

--- drex/utils/tool_functions.py
+++ drex/utils/tool_functions.py
@@ -1,9 +1,9 @@
 # To use: python3 tool_functions.py
 
-from poibin import PoiBin
+from drex.utils.poibin import PoiBin
 import numpy as np
 import sys
-from load_data import RealRecords
+from drex.utils.load_data import RealRecords
 
 
 # Return the estimated time cost of chunking and replicating a data of 
@@ -111,36 +111,3 @@ def get_max_K_from_reliability_threshold_and_nodes(number_of_nodes, reliability_
 		print("Biggest K we can choose to meet the reliability threshold with N =", number_of_nodes, "is", max_K)
 	
 	return max_K
-
-
-# Under are just some values and examples on how to use the utils functions
-
-# Numpy arrays of probability of failure each node over the data timeframe
-# TODO use real values and have them as external inputs
-p = np.array([0.01, 0.2, 0.1, 0.1, 0.1, 0.3])
-
-# Bandwidth to write on the storage nodes in MB/s
-# TODO use real values and have them as external inputs
-bandwidths = np.array([10, 23, 15, 13, 11, 32])
-
-# Number of nodes
-# TODO have it as external input
-N = 6
-
-# Threshold we want to meet
-# TODO have it as external input
-reliability_threshold = 0.99
-
-# To manage the real time obtained in experiments
-real_records = RealRecords()
-
-# File size in MB
-# TODO have it as external input and have different values depending on the data type
-file_size = 10
-
-print("Probability of availability must be superior to", reliability_threshold)
-
-K = get_max_K_from_reliability_threshold_and_nodes(N, reliability_threshold, p)
-
-set_of_N_on_pareto = get_set_of_N_on_pareto_front(N, reliability_threshold, p, file_size, bandwidths, real_records)
-print("The set of N on the pareto front between speed and size is", set_of_N_on_pareto)
