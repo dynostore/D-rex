0fdb1e6383b0fde9b6b97794a4beee087a7cdb88
gonthier <maxjp.gr@gmail.com>
2024-04-18 13:35:26 -0500

    start of code for reduced complexity

--- test/drex-test.py
+++ test/drex-test.py
@@ -16,11 +16,16 @@ number_of_nodes = 10
 print("There are", number_of_nodes, "nodes.")
 
 # Numpy arrays of probability of failure each node over the data timeframe
-# ~ p = np.array([0.01, 0.2, 0.1, 0.1, 0.1, 0.3, 0.1, 0.01, 0.5, 0.6])
-p = [0.1] * number_of_nodes
+p = np.array([0.01, 0.2, 0.1, 0.1, 0.1, 0.3, 0.1, 0.01, 0.5, 0.6])
+# p = [0.1] * number_of_nodes
 
 # Bandwidth to write on the storage nodes in MB/s
-bandwidths = [20] * number_of_nodes
+bandwidths = np.array([20, 10, 15, 18, 21, 20, 10, 10, 20, 5])
+# bandwidths = [20] * number_of_nodes
+
+# Storage size of each node
+node_sizes = np.array([100, 100, 100, 100, 200, 120, 160, 190, 220, 100])
+# node_sizes = [100] * number_of_nodes
 
 # Threshold we want to meet
 reliability_threshold = 0.6
@@ -39,6 +44,15 @@ if (file_size <= 0 or number_of_nodes < 3):
 for i in range(3, number_of_nodes):
 	print(i,i-2,replication_and_chuncking_time(i, 2, file_size, bandwidths[:i], real_records))
 
+# For reduced complexity we need to call this function that groups similar nodes together
+matrix_of_differences = group_nodes_by_similarities(number_of_nodes, p, bandwidths, node_sizes)
+
+#We need to allow a maximum difference allowed to consider two nodes are similar
+maximum_difference_allowed = 0.10 # Here it is 10%
+
+# Then we get a reduced set of nodes from this
+reduced_set_of_nodes, reduced_set_of_nodes_first_nodes_only = get_reduced_set_of_nodes(number_of_nodes, matrix_of_differences, maximum_difference_allowed)
+
 # Algorithm 1
 # Time for 10 nodes: 0 seconds
 # Time for 100 nodes: 0 seconds
@@ -53,6 +67,7 @@ for i in range(3, number_of_nodes):
 # Time for 20 nodes: 279 seconds
 # Time for 22 nodes: 1280 seconds
 # ~ algorithm2(number_of_nodes, p, bandwidths, reliability_threshold, file_size, real_records)
+algorithm2_reduced_complexity(number_of_nodes, p, bandwidths, reliability_threshold, file_size, real_records, reduced_set_of_nodes, reduced_set_of_nodes_first_nodes_only)
 
 # Algorithm 3
 # Time for 16 nodes: 16 seconds
@@ -60,4 +75,4 @@ for i in range(3, number_of_nodes):
 # ~ algorithm3(number_of_nodes, p, bandwidths, reliability_threshold, file_size, real_records)
 
 # Random scheduler
-random_schedule(number_of_nodes, p, reliability_threshold)
+# random_schedule(number_of_nodes, p, reliability_threshold)
