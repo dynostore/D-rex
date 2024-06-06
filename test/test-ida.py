e77e906a33bebc2c46e65b8ebf92f2020cd57b8e
ddomizzisg <ddomizzisag@gmail.com>
2024-06-06 11:35:20 +0200

    simulation from trace

--- test/test-ida.py
+++ test/test-ida.py
@@ -10,8 +10,10 @@ from drex.utils.reliability import ida
 def generate_random_bytes(n):
     return os.urandom(n)
 
+#Data in MB
+size = 1000
 
-data = generate_random_bytes(10)
+data = generate_random_bytes(size * 1024 * 1024)
 #variable = 4
 #data = variable.to_bytes(2, 'big') 
 
@@ -22,57 +24,8 @@ m = 3
 #print(data)
 start = time.time_ns()/1e+6
 
-# store data in a file
-with open("original.txt", "wb") as f:
-    f.write(data)
-
 disp_result = ida.split_bytes(data, n, m)
 
-#store fragments in a file
-for d in range(n):
-    with open("fragment"+str(d)+".txt", "wb") as f:
-        f.write(disp_result[d].content)
+end = time.time_ns()/1e+6
 
-#res2 = ida.split_bytes_v0(data, n, m)
-#to_recover = disp_result[0:m]
-#print(to_recover)
-result = ida.assemble_bytes(disp_result)
-
-
-#Get hash of the original data
-original_hash = hashlib.sha224(data).digest()
-
-#Get hash of the recovered data
-recovered_hash = hashlib.sha224(result).digest()
-
-print("Original hash:", original_hash)
-print("Recovered hash:", recovered_hash)
-print("Hashes match:", original_hash == recovered_hash)
-print(data)
-print(result)
-
-
-#print(ida.assemble_bytes(res2))
-
-#print(len(disp_result))
-
-# total = 0
-# total2 = 0
-# for d in range(m):
-#     total += len(disp_result[d].content)
-#     total2 += sys.getsizeof(disp_result[d].content)
-#     print("content",len(disp_result[d].content))
-# #print(disp_result)
-
-# print("total", total)
-# print("total2", total2)
-# print("data size",len(data))
-# print("expected",len(data) / m * n)
-
-
-#fragments = ida.split_bytes_v2(data, n, m)
-#res = ida.merge_bytes_v2(fragments, n, m)
-#fragments = ida.split_bytes_v0(data, n, m)
-#result = ida.assemble_bytes(fragments)
-#print(result)
-
+print("Time: ", end - start)
