7c416b8548368ba865356aca133120cd8d027a87
gonthier <maxjp.gr@gmail.com>
2024-07-30 15:50:50 -0500

    more code in C

--- drex/schedulers/algorithm4.c
+++ drex/schedulers/algorithm4.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// Global variable for the number of nodes
-int number_of_nodes = 0;
+#include <limits.h>
+#include <math.h>
+#include <time.h>
 
 typedef struct {
     int id;
-    long storage_size;
+    double storage_size;
     int write_bandwidth;
     int read_bandwidth;
-    float annual_failure_rate;
+    double probability_failure;
 } Node;
 
 // Function to count the number of nodes in the file
-void count_nodes(const char *filename) {
+int count_nodes(const char *filename) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
@@ -31,7 +31,7 @@ void count_nodes(const char *filename) {
     fclose(file);
 
     // Subtract 1 if the first line is a header
-    number_of_nodes = count - 1;  // Adjust if there is no header
+    return count - 1;  // Adjust if there is no header
 }
 
 // Function to count the number of lines with Access Type 2
@@ -69,8 +69,22 @@ int count_lines_with_access_type(const char *filename) {
     return count;
 }
 
+// Function to calculate the probability of failure over a given period given the annual failure rate
+double probability_of_failure(double failure_rate, int data_duration_on_system) {
+    // Convert data duration to years
+    double data_duration_in_years = data_duration_on_system / 365.0;
+    
+    // Convert failure rate to a fraction
+    double lambda_rate = failure_rate / 100.0;
+    
+    // Calculate the probability of failure
+    double probability_failure = 1 - exp(-lambda_rate * data_duration_in_years);
+    
+    return probability_failure;
+}
+
 // Function to read data from file and populate the nodes array
-void read_node(const char *filename, Node *nodes) {
+void read_node(const char *filename, int number_of_nodes, Node *nodes, int data_duration_on_system, long* max_node_size, long* total_storage_size, double* initial_node_sizes) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
@@ -83,20 +97,30 @@ void read_node(const char *filename, Node *nodes) {
 
     // Read the file line by line and populate nodes array
     int index = 0;
-    while (fscanf(file, "%d,%ld,%d,%d,%f",
+    while (fscanf(file, "%d,%lf,%d,%d,%lf",
                   &nodes[index].id,
                   &nodes[index].storage_size,
                   &nodes[index].write_bandwidth,
                   &nodes[index].read_bandwidth,
-                  &nodes[index].annual_failure_rate) == 5) {
+                  &nodes[index].probability_failure) == 5) {
+        if (nodes[index].storage_size > *max_node_size)
+        {
+            *max_node_size = nodes[index].storage_size;
+        }
+        *total_storage_size += nodes[index].storage_size;
+        initial_node_sizes[index] = nodes[index].storage_size;
         index++;
+    }
+    // Update the annual failure rate to become the probability of failure of the node
+    for (int i = 0; i < number_of_nodes; i++) {
+        nodes[i].probability_failure = probability_of_failure(nodes[i].probability_failure, data_duration_on_system);
     }
 
     fclose(file);
 }
 
 // Function to read data from file and populate the sizes array
-void read_data(const char *filename, float *sizes, int *size_count) {
+void read_data(const char *filename, double *sizes, int *size_count) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
@@ -132,21 +156,45 @@ void read_data(const char *filename, float *sizes, int *size_count) {
     fclose(file);
 }
 
+void algorithm4(int number_of_nodes, Node *nodes, float reliability_threshold, double size, long max_node_size, int min_data_size, long total_storage_size, int *N, int *K, double* total_storage_used, double* total_upload_time, double* total_parralelized_upload_time, int* number_of_data_stored, double* total_scheduling_time, int* total_N) {
+    
+    clock_t start, end;
+    
+    start = clock();
+    *N = 4;
+    *K = 2;
+    end = clock();
+        
+    if (*N != -1) {
+        *number_of_data_stored += 1;
+        *total_N += *N;
+        *total_storage_used += (size / *K) * *N;
+        // TODO
+        //~ total_upload_time +=
+        //~ total_parralelized_upload_time += 
+    }
+    *total_scheduling_time += ((double) (end - start)) / CLOCKS_PER_SEC;
+}
+
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s <input_node> <input_data>\n", argv[0]);
+    int i = 0;
+    if (argc < 5) {
+        fprintf(stderr, "Usage: %s <input_node> <input_data> <data_duration_on_system> <reliability_threshold>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
     const char *input_node = argv[1];
     const char *input_data = argv[2];
-
-    // Step 1: Count the number of lines with Access Type 2
+    int data_duration_on_system = atoi(argv[3]);
+    double reliability_threshold = atof(argv[4]);
+    printf("Data have to stay %d days on the system. Reliability threshold is %f\n", data_duration_on_system, reliability_threshold);
+    
+    // Step 1: Count the number of lines
     int count = count_lines_with_access_type(input_data);
-    count_nodes(input_node);
+    int number_of_nodes = count_nodes(input_node);
 
-    // Step 2: Allocate memory for the sizes array
-    float *sizes = (float *)malloc(count * sizeof(float));
+    // Step 2: Allocate memory
+    double *sizes = (double*)malloc(count * sizeof(double));
     if (sizes == NULL) {
         perror("Error allocating memory");
         return EXIT_FAILURE;
@@ -157,28 +205,69 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    // Step 3: Read data into the sizes array
+    // Step 3: Read data into the arrays
     int size_count;
     read_data(input_data, sizes, &size_count);
-    read_node(input_node, nodes);
-
-    // Print the collected sizes
-    printf("Sizes with Access Type 2:\n");
-    for (int i = 0; i < size_count; i++) {
+    long total_storage_size = 0;
+    long max_node_size = 0;
+    double *initial_node_sizes = (double*)malloc(number_of_nodes * sizeof(double));
+    read_node(input_node, number_of_nodes, nodes, data_duration_on_system, &max_node_size, &total_storage_size, initial_node_sizes);
+    
+    // Print the collected data
+    #ifdef PRINT
+    printf("There are %d data in W mode:\n", size_count);
+    for (i = 0; i < size_count; i++) {
         printf("%.2f\n", sizes[i]);
     }
-    for (int i = 0; i < number_of_nodes; i++) {
-        printf("Node %d: storage_size=%ld, write_bandwidth=%d, read_bandwidth=%d, annual_failure_rate=%.2f\n",
+    for (i = 0; i < number_of_nodes; i++) {
+        printf("Node %d: storage_size=%f, write_bandwidth=%d, read_bandwidth=%d, probability_failure=%f\n",
                nodes[i].id, nodes[i].storage_size, nodes[i].write_bandwidth,
-               nodes[i].read_bandwidth, nodes[i].annual_failure_rate);
+               nodes[i].read_bandwidth, nodes[i].probability_failure);
     }
-
-    set_of_nodes_chosen, N, K, node_sizes = algorithm4(number_of_nodes, reliability_nodes,write_bandwidths, reliability_threshold, data, real_records, node_sizes, max_node_size,min_data_size, system_saturation, total_storage_size, predictor)
-
+    printf("Max node size is %ld\n", max_node_size);
+    printf("Total storage size is %ld\n", total_storage_size);
+    #endif
+    
+    // Variables used in algorithm4
+    int min_data_size = INT_MAX;
+    int N;
+    int K;
+    const char *output_filename = "output_drex_only.csv";
+    const char *alg_to_print = "Algorithm4";
+    double total_scheduling_time = 0;
+    double total_storage_used = 0;
+    double total_upload_time = 0;
+    double total_parralelized_upload_time = 0;
+    int number_of_data_stored = 0;
+    int total_N = 0; // Number of chunks
+    
+    for (i = 0; i < size_count; i++) {
+        if (min_data_size > sizes[i]) {
+            min_data_size = sizes[i];
+        }
+        algorithm4(number_of_nodes, nodes, reliability_threshold, sizes[i], max_node_size, min_data_size, total_storage_size, &N, &K, &total_storage_used, &total_upload_time, &total_parralelized_upload_time, &number_of_data_stored, &total_scheduling_time, &total_N);
+        printf("Algorithm 4 chose N = %d and K = %d\n", N, K);
+    }
+    #ifdef PRINT
+    printf("Total scheduling time was %f\n", total_scheduling_time);
+    #endif
+    
     // Free allocated memory
     free(sizes);
     free(nodes);
-
+    
+    // Writting the outputs
+    FILE *file = fopen(output_filename, "a");
+    if (file == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+    fprintf(file, "%s, %f, %f, %f, %f, %d, %d, %f, %f, %f, \"[", alg_to_print, total_scheduling_time, total_storage_used, total_upload_time, total_parralelized_upload_time, number_of_data_stored, total_N, total_storage_used / number_of_data_stored, total_upload_time / number_of_data_stored, (double)total_N / number_of_data_stored);
+    for (i = 0; i < number_of_nodes - 1; i++) {
+        fprintf(file, "%f, ", initial_node_sizes[i]);
+    }
+    fprintf(file, "%f]\", \"[", initial_node_sizes[i]);
+        
     return EXIT_SUCCESS;
 }
 
