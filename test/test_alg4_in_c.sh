bfcb5bd4ac2945e5a71a410a42190b5447d930b8
gonthier <maxjp.gr@gmail.com>
2024-08-16 17:12:09 -0500

    maj

--- test/test_alg4_in_c.sh
+++ test/test_alg4_in_c.sh
@@ -5,9 +5,9 @@ truncate -s 0 output_drex_only.csv
 make
 #~ ./alg4 drex/inputs/nodes/10_most_reliable_nodes.csv drex/inputs/data/test.csv 365.0 0.99 2 4
 #~ ./alg4 drex/inputs/nodes/10_most_reliable_nodes.csv drex/inputs/data/test.csv 365.0 0.99 2 4 drex/inputs/nodes/no_supplementary_nodes.csv
-#~ ./alg4 drex/inputs/nodes/10_most_reliable_nodes.csv drex/inputs/data/test.csv 365.0 0.99 2 4 drex/inputs/nodes/add_node_pattern_1.csv
+./alg4 drex/inputs/nodes/test.csv drex/inputs/data/test.csv 365.0 0.99 2 4 drex/inputs/nodes/add_node_pattern_1.csv
 #~ ./alg4 drex/inputs/nodes/all_nodes_backblaze.csv drex/inputs/data/test.csv 365.0 0.99 2 4 drex/inputs/nodes/add_node_pattern_1.csv
-./alg4 drex/inputs/nodes/all_nodes_backblaze.csv drex/inputs/data/test.csv 365.0 0.99 2 4 drex/inputs/nodes/no_supplementary_nodes.csv
+#~ ./alg4 drex/inputs/nodes/all_nodes_backblaze.csv drex/inputs/data/test.csv 365.0 0.99 2 4 drex/inputs/nodes/no_supplementary_nodes.csv
 #~ ./alg4 drex/inputs/nodes/10_most_unreliable_nodes.csv drex/inputs/data/test.csv 365.0 0.99 2 4
 #~ ./alg4 drex/inputs/nodes/10_most_reliable_nodes.csv drex/inputs/data/test.csv 365.0 0.99 2 2
 #~ ./alg4 drex/inputs/nodes/10_most_reliable_nodes.csv drex/inputs/data/MEVA_merged.csv 365 0.99 2 4
